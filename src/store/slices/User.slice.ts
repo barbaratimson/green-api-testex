26472ddb590da3d7e0bb59f3cc0b882f2b9ca4df
barbaratimson <cokolek.sokol2014@mail.ru>
2025-02-06 17:19:03 +0700

    fix

--- src/store/slices/User.slice.ts
+++ src/store/slices/User.slice.ts
@@ -1,11 +1,13 @@
 import {createSlice, current, PayloadAction} from '@reduxjs/toolkit'
 
+const savedLocalUser = localStorage.getItem("currentUser")
+const savedCurrentUser: userState = savedLocalUser && JSON.parse(localStorage.getItem("currentUser"))
+
 export interface userState {
     idInstance: string;
     apiTokenInstance: string;
 }
 
-const savedCurrentUser = JSON.parse(localStorage.getItem("currentUser") ?? "")
 export const initialState: userState = {
     idInstance: savedCurrentUser ? savedCurrentUser.idInstance : "",
     apiTokenInstance: savedCurrentUser ? savedCurrentUser.apiTokenInstance : ""
